de12081ef43c023cfe7e0f2b0c8dde9709e77703
Kiki <147945185+ikyy19999@users.noreply.github.com>
2026-09-04 08:18:43 +0700

    Add files via upload

--- src/data/translations.js
+++ src/data/translations.js
@@ -152,7 +152,6 @@ const translations = {
         "Operating System": "Operating System",
         "LAN/WAN & Troubleshooting": "LAN/WAN & Troubleshooting",
         "Installation & Maintenance": "Installation & Maintenance",
-        "Problem Solving": "Problem Solving",
       },
       tags: {
         Project: "Project",
@@ -232,6 +231,8 @@ const translations = {
       availableFor: "Available for",
       availableText:
         "Freelance projects, full stack development, and collaboration.",
+      status: "Status",
+      statusText: "Currently available for work",
       connect: "Connect",
       socialLinks: "Social links",
       directEmail: "Direct email",
@@ -418,7 +419,6 @@ const translations = {
         "Operating System": "Sistem Operasi",
         "LAN/WAN & Troubleshooting": "LAN/WAN & Troubleshooting",
         "Installation & Maintenance": "Instalasi & Maintenance",
-        "Problem Solving": "Pemecahan Masalah",
       },
       tags: {
         Project: "Proyek",
@@ -498,6 +498,8 @@ const translations = {
       availableFor: "Terbuka untuk",
       availableText:
         "Proyek freelance, full stack development, dan kolaborasi.",
+      status: "Status",
+      statusText: "Saat ini tersedia untuk pekerjaan baru",
       connect: "Terhubung",
       socialLinks: "Media sosial",
       directEmail: "Email langsung",
@@ -684,7 +686,6 @@ const translations = {
         "Operating System": "Sistem Operasi",
         "LAN/WAN & Troubleshooting": "LAN/WAN & Troubleshooting",
         "Installation & Maintenance": "Instalasi & Maintenance",
-        "Problem Solving": "Ngatasi Masalah",
       },
       tags: {
         Project: "Karya",
@@ -763,6 +764,8 @@ const translations = {
         "Menawi panjenengan gadhah proyek, ide produk, utawi rencana kolaborasi, mangga kirim rinciyanipun lan kula badhe ngubungi panjenengan.",
       availableFor: "Saged kangge",
       availableText: "Proyek freelance, full stack development, saha kolaborasi.",
+      status: "Status",
+      statusText: "Sapunika saged nampi pakaryan",
       connect: "Sesambungan",
       socialLinks: "Pranala sosial",
       directEmail: "Email langsung",
